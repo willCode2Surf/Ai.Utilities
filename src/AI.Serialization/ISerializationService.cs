6ffe7b742a9f13961341bc74a235eb6efc18a3c2
Sam Martindale <sam.martindale@theAIteam.com>
2013-01-08 13:26:30 -0600

    more stuff

---
+++ src/AI.Serialization/ISerializationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Serialization
+{
+    public interface ISerializationService<T>
+        where T: class
+    {
+        T Get(string serializedString);
+
+        string Put(T objectToSerialize);
+    }
+}
---
+++ src/AI.Serialization/SerializationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Serialization
+{
+    public class SerializationService<T> : ISerializationService<T>
+        where T : class
+    {
+        public T Get(string serializedString)
+        {
+            
+        }
+
+        public string Put(T objectToSerialize)
+        {
+            
+        }
+    }
+}
